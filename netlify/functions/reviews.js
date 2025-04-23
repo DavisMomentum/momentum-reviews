8c982580df57439ac359dcdd273c803ec2342b08
DavisMomentum <davidsmleye@gmail.com>
2025-04-23 00:49:43 -0400

    Update reviews.js to use MY_AWS_SECRET_ACCESS_KEY_ID

--- netlify/functions/reviews.js
+++ netlify/functions/reviews.js
@@ -8,7 +8,7 @@ const s3Client = new S3Client({
     region: process.env.AWS_REGION,
     credentials: {
         accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+        secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY
     }
 });
 const bucketName = process.env.BUCKET_NAME;
